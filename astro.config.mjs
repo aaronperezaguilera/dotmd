f3b6a4372b48b8f3702245884676861a15ebe9ac
Aaron <aaronperezaguilera@gmail.com>
2024-05-07 17:28:16 +0200

    chore: Update Astro adapter for Cloudflare to use directory mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({ mode: "directory" }), 
 });
