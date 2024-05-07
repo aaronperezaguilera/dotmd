74bebdc680caebed726ad7c93e587f5c2eb3912f
Aaron <aaronperezaguilera@gmail.com>
2024-05-07 17:41:31 +0200

    chore: Update Astro adapter for Cloudflare and remove unused dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-  vite: {
-    ssr: {
-      external: ['node:buffer'],
-    },
-  },
-});
+export default defineConfig();
