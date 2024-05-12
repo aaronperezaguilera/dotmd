ac69d1ee6e775f7361db0e0a3306aca2ece732ae
Aaron <aaronperezaguilera@gmail.com>
2024-05-12 16:39:29 +0200

    refactor: Update blog post layout and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: {
+        primary: '#232323',
+        secondary: '#282828',
+        hover: '#2e2e2e'
+      },
+      borderColor: {
+        primary: '#343434'
+      },
+      fill: {
+        primary: '#232323'
+      },
+      textColor: {
+        primary: '#ffffff',
+        secondary: '#a0a0a0'
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography'), require('tailwindcss-animated')]
 }
